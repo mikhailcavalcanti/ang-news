c51ed68aea50b9d85f8c4102d19a99f73ea20fce
Mikhail Cavalcanti <mikhailcavalcanti@gmail.com>
2014-06-10 17:44:59 -0300

    Criado Post Service
    - criado o serviço rest de Posts, usando o firebase.com

---
+++ app/scripts/services/post.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name angNewsApp.Post
+ * @description
+ * # Post
+ * Factory in the angNewsApp.
+ */
+/* global app */
+app.factory('Post', function ($resource) {
+  return $resource('https://ang-news-fiesh.firebaseio.com/posts:id.json');
+});
