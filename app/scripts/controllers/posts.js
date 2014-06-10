7f04d420f0cbf6d793ab3a082cab65eed0af3816
Mikhail Cavalcanti <mikhailcavalcanti@gmail.com>
2014-06-10 18:10:51 -0300

    Adaptado save, delete post para usar api
    - adaptado os métodos do PostsCtrl para usar o serviço do firebase

--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -1,16 +1,20 @@
 'use strict';
 
 /* global app */
-app.controller('PostsCtrl', function ($scope) {
-  $scope.posts = [];
+app.controller('PostsCtrl', function ($scope, Post) {
+  $scope.posts = Post.get();
   $scope.post = {url: 'http://', title: ''};
 
   $scope.submitPost = function() {
-    $scope.posts.push($scope.post);
+    Post.save($scope.post, function(ref) {
+      $scope.posts[ref.name] = $scope.post;
+    });
     $scope.post = {url: 'http://', title: ''};
   };
 
-  $scope.deletePost = function(index) {
-    $scope.posts.splice(index);
+  $scope.deletePost = function(postId) {
+    Post.delete({id: postId}, function() {
+      delete $scope.posts[postId];
+    });
   };
 });
