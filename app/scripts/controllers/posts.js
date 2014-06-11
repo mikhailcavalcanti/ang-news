1307324580a4d51a715ec55f79f85b76cafa99aa
Mikhail Cavalcanti <mikhailcavalcanti@gmail.com>
2014-06-11 12:10:06 -0300

    Adaptado PostsCtrl para firebase
    - adaptado o controller para usar o serviço (factory) do firebase, 3 way data bind

--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -8,19 +8,16 @@
 * Controller of the angNewsApp
 */
 app.controller('PostsCtrl', function ($scope, Post) {
-  $scope.posts = Post.get();
+  $scope.posts = Post.all;
   $scope.post = {url: 'http://', title: ''};
 
-  $scope.submitPost = function() {
-    Post.save($scope.post, function(ref) {
-      $scope.posts[ref.name] = $scope.post;
+  $scope.submitPost = function () {
+    Post.create($scope.post).then(function () {
+      $scope.post = {url: 'http://', title: ''};
     });
-    $scope.post = {url: 'http://', title: ''};
   };
 
-  $scope.deletePost = function(postId) {
-    Post.delete({id: postId}, function() {
-      delete $scope.posts[postId];
-    });
+  $scope.deletePost = function (postId) {
+    Post.delete(postId);
   };
 });
