61b3ec351f6d5a477b46ae23e838a7e42f5ffafb
Mikhail Cavalcanti <mikhailcavalcanti@gmail.com>
2014-06-13 11:22:36 -0300

    Revert "Indentação app.js"
    This reverts commit 6fbb4983a83b9e00d237b6dca9b4cb1c69a12177.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,8 +8,8 @@
  *
  * Main module of the application.
  */
-var app = angular;
-app.module('angNewsApp', [
+var app = angular
+  .module('angNewsApp', [
     'ngAnimate',
     'ngCookies',
     'ngResource',
@@ -17,27 +17,27 @@ app.module('angNewsApp', [
     'ngSanitize',
     'ngTouch',
     'firebase'
-    ])
-.constant('FIREBASE_URL', 'https://ang-news-fiesh.firebaseio.com')
-.config(function ($routeProvider) {
-  $routeProvider
-  .when('/', {
-    templateUrl: 'views/posts.html',
-    controller: 'PostsCtrl'
-  })
-  .when('/posts/:postId', {
-    templateUrl: 'views/post-view.html',
-    controller: 'PostViewCtrl'
-  })
-  .when('/register', {
-    templateUrl: 'views/register.html',
-    controller: 'AuthCtrl'
-  })
-  .when('/login', {
-    templateUrl: 'views/login.html',
-    controller: 'AuthCtrl'
-  })
-  .otherwise({
-    redirectTo: '/'
+  ])
+  .constant('FIREBASE_URL', 'https://ang-news-fiesh.firebaseio.com');
+app.config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/posts.html',
+        controller: 'PostsCtrl'
+      })
+      .when('/posts/:postId', {
+        templateUrl: 'views/post-view.html',
+        controller: 'PostViewCtrl'
+      })
+      .when('/register', {
+        templateUrl: 'views/register.html',
+        controller: 'AuthCtrl'
+      })
+      .when('/login', {
+        templateUrl: 'views/login.html',
+        controller: 'AuthCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
   });
-});
