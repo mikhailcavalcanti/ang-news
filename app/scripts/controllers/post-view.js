f77baa283088529531fb52bb924325760c28b796
Mikhail Cavalcanti <mikhailcavalcanti@gmail.com>
2014-06-18 12:26:21 -0300

    Votação
    - implementada a votação de comentários

--- app/scripts/controllers/post-view.js
+++ app/scripts/controllers/post-view.js
@@ -14,7 +14,32 @@ app.controller('PostViewCtrl', function ($scope, $routeParams, Post) {
         Post.addComment($routeParams.postId, $scope.comment);
         $scope.comment = '';
     };
+
     $scope.removeComment = function (comment, commentId) {
       Post.deleteComment($scope.post, comment, commentId);
+    };
+
+    $scope.upVotePost = function (upVoted) {
+      if (upVoted) {
+        Post.clearVote($routeParams.postId, true);
+      } else {
+        Post.upVote($routeParams.postId);
+      }
+    };
+
+    $scope.downVotePost = function (downVoted) {
+      if (downVoted) {
+        Post.clearVote($routeParams.postId, false);
+      } else {
+        Post.downVote($routeParams.postId);
+      }
+    };
+
+    $scope.upVoted = function () {
+      return Post.upVoted($scope.post);
+    };
+
+    $scope.downVoted = function () {
+      return Post.downVoted($scope.post);
     };
 });
